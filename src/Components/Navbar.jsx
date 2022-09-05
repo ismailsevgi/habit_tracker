efc251a5244f0d85e65b1fd44cdaa9a4862939d4
ismail sevgi <ismailsevgi95@gmail.com>
2022-09-06 02:48:40 +0300

    Changes: Logo, responsive navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import '../CSS-Files/Navbar.css';
-import Logo from '../Logo/newLogo.jpg';
+
 import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className='container'>
-      <div className='row nav'>
-        <img className='col-sm-4' src={Logo} />
+    <div className='navbar'>
+      <div className='container-fluid'>
+        <div className='nav'>
+          <div className='logo'>
+            <NavLink to='/habits'>
+              <h1>HABIT</h1>
+              <h1>TRACKER</h1>
+            </NavLink>
+          </div>
+          <div className='rowLinks'>
+            <NavLink className='link' to='/habits'>
+              Habits
+            </NavLink>
+            <NavLink className='link' to='/performance'>
+              Performance
+            </NavLink>
 
-        <div className='col-sm-8 rowLinks'>
-          <NavLink className='col-3 Logo' to='/habits'>
-            Habits
-          </NavLink>
-          <NavLink className='col-3 Logo' to='/performance'>
-            Performance
-          </NavLink>
-          <NavLink className='col-3 Logo' to='/notes/tasks'>
-            Notes
-          </NavLink>
-          <NavLink className='col-3 Logo' to='/focus'>
-            Focus
-          </NavLink>
+            <NavLink className='link' to='/notes/tasks'>
+              Notes
+            </NavLink>
+            <NavLink className='link' to='/focus'>
+              Focus
+            </NavLink>
+          </div>
         </div>
       </div>
     </div>
